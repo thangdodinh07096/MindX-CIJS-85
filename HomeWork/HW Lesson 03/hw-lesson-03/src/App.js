6b2249667b7e401aafc4eda42654872932b7cceb
thangdodinh07096 <thangdodinh07096>
2023-06-23 01:56:17 +0700

    homework lesson 03

---
+++ HomeWork/HW Lesson 03/hw-lesson-03/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Register from './Auth/Register/Register';
+
+function App() {
+  return (
+    <div className="App">
+      <Register />
+    </div>
+  );
+}
+
+export default App;
